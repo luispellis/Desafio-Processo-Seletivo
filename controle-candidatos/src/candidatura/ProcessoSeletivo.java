f52273f6a01a83868be85ecec04783f0ce05b9e9
luisp <luisfelipe_pellis@hotmail.com>
2024-10-09 21:54:47 -0300

    atualizando com analise de candidato dois metodos para analisar um candidato aleatório e com os respectivos valor correto selecionar automaticamente conforme o valor base

--- controle-candidatos/src/candidatura/ProcessoSeletivo.java
+++ controle-candidatos/src/candidatura/ProcessoSeletivo.java
@@ -1,16 +1,45 @@
+import java.util.concurrent.ThreadLocalRandom;
+
 public class ProcessoSeletivo {
 
     // Vamos explorar alguns outros cenários com fluxo condicionais, repetições e excepcionais.
+    
+    public static void main(String[] args) {
+        selecaoCandidatos();
+    }
+
+    // Case 2: Foi solicitado que o nosso sistema garanta que diante das inúmeras candidaturas sejam selecionados apenas no máximo 5 candidatos
+    // para a entrevista onde o salário pretendido seja menor ou igual ao salário base.
+
+    static void selecaoCandidatos(){
+        String [] candidatos = {"FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO", "MONICA", "FABRICIO", "MIRELA", "DANIELA", "JORGE",};
+
+        int candidatosSelecionados = 0;
+        int candidatosAtual = 0;
+        double salarioBase = 2000.0;
+
+        while(candidatosSelecionados < 5 && candidatosAtual < candidatos.length){
+            String candidato = candidatos[candidatosAtual];
+            double salarioPretendido = valorPretendido();
+
+            System.out.println("O Candidato " + candidato + "solicitou este valor de salário " + salarioPretendido);
+            if(salarioBase >= salarioPretendido){
+                System.out.println("O Candidato " + candidato + "foi selecionado para a vaga");
+                candidatosSelecionados++;
+            }
+            candidatosAtual++;
+        }
+    }
+
+    static double valorPretendido(){
+        return ThreadLocalRandom.current().nextDouble(1800, 2200);
+    }
+
 
     // Case 1: Vamos imaginar que em um processo seletivo existe o valor base salarial de R$ 2.000,00 e o salário pretendido pelo candidato. Vamos elaborar um controle de fluxo onde:
-
     // Se o valor salario base for maior que o valor salario pretendido, imprima: "LIGAR PARA O CANDIDATO";
     // Senão se o valor salario base for igual ao valor salario pretendido, imprima: "LIGAR PARA O CANDIDATO COM CONTRA PROPOSTA";
     // Senão imprima: "AGUARDANDO RESULTADO DEMAIS CANDIDATOS";
-    
-    public static void main(String[] args) {
-        analisarCandidato(1900.0);
-    }
 
     static void analisarCandidato(double salarioPretendido){
         double salarioBase = 2000.0; 
